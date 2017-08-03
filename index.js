818d915bb16e80a322efb0ce8241e2407790acf0
Adrian Stabiszewski <as@grundid.de>
2017-08-03 17:24:41 +0200

    added demo bot

---
+++ index.js
@@ -0,0 +1,11 @@
+const Telegraf = require('telegraf');
+
+console.log("Running bot with token: ", process.env.BOT_TOKEN);
+const app = new Telegraf(process.env.BOT_TOKEN);
+app.command('start', ({from, reply}) => {
+    console.log('start', from);
+    return reply('Welcome!');
+});
+app.hears('hi', (ctx) => ctx.reply('Hey there!'));
+app.on('sticker', (ctx) => ctx.reply('👍'));
+app.startPolling();
