de5e46fcfef799648ce67488e8121c9b46fcd962
Adrian Stabiszewski <as@grundid.de>
2017-08-03 18:16:49 +0200

    update interval

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const Telegraf = require('telegraf');
 
+let chatRoomId = null;
+
 console.log("Running bot with token: ", process.env.BOT_TOKEN);
-const app = new Telegraf(process.env.BOT_TOKEN);
-app.command('start', ({from, reply}) => {
+const app = new Telegraf(process.env.BOT_TOKEN, {username: "FoodSupply_bot"});
+app.command('start', ({from, chat, reply}) => {
     console.log('start', from);
+    console.log('chat', chat);
+    chatRoomId = chat.id;
     return reply('Welcome!');
 });
-app.hears('hi', (ctx) => ctx.reply('Hey there!'));
+
+app.hears('hi', (ctx) => {
+    console.log("ctx: ", ctx);
+    ctx.reply('Hey there!')
+});
+
+setInterval(function () {
+    if (chatRoomId !== null) {
+        app.telegram.sendMessage(chatRoomId, "Hat jemand Hunger???");
+    }
+}, 60000);
 app.on('sticker', (ctx) => ctx.reply('👍'));
 app.startPolling();
