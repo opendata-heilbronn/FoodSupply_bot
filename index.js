db68cbd6ae8381d62a9328a0eb5e0715aa195715
Adrian Stabiszewski <as@grundid.de>
2017-08-03 19:18:53 +0200

    added sqlite db

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const Telegraf = require('telegraf');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('foodsupply.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, function (error) {
+    if (error === null) {
+        db.serialize(function () {
+            db.run("CREATE TABLE IF NOT EXISTS chats (chatId TEXT, created DATETIME)");
+            db.run("CREATE TABLE IF NOT EXISTS orders (order_id INTEGER PRIMARY KEY AUTOINCREMENT, chatId TEXT, created DATETIME)");
+            db.run("CREATE TABLE IF NOT EXISTS user_stats (user_order_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "userId TEXT, ORDER_ID INTEGER, " +
+                "order_time DATETIME, order_type VARCHAR(32), amount NUMERIC)");
+        });
+    }
+});
+
 
 let chatRoomId = null;
 
