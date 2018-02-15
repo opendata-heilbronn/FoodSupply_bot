8f3286939ed6399cdfab2fd928cb2b853069f87b
Patrick <patrick2003>
2018-02-15 20:15:28 +0100

    added firebase

--- functions/foodsupplybot.js
+++ functions/foodsupplybot.js
@@ -1,19 +1,6 @@
 const Telegraf = require('telegraf');
 const { Markup } = Telegraf;
 const messages = require('./messages');
-/*const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('foodsupply.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, function (error) {
-    if (error === null) {
-        db.serialize(function () {
-            db.run('CREATE TABLE IF NOT EXISTS chats (chatId TEXT, created DATETIME)');
-            db.run('CREATE TABLE IF NOT EXISTS orders (order_id INTEGER PRIMARY KEY AUTOINCREMENT, chatId TEXT, created DATETIME)');
-            db.run('CREATE TABLE IF NOT EXISTS user_stats (user_order_id INTEGER PRIMARY KEY AUTOINCREMENT, ' +
-                'userId TEXT, ORDER_ID INTEGER, ' +
-                'order_time DATETIME, order_type VARCHAR(32), amount NUMERIC)');
-        });
-    }
-});*/
-
 
 const questions = {
     subway_vote: {
@@ -30,7 +17,7 @@ const questions = {
             [{ text: 'Joni', callback: 'pizza_iwant_Joni' }, { text: 'Hawaii', callback: 'pizza_iwant_Hawaii' }],
             [{ text: 'Pilze', callback: 'pizza_iwant_Pilze' }, { text: 'Sucuk', callback: 'pizza_iwant_Sucuk' }],
             [{ text: 'Pepperoni', callback: 'pizza_iwant_Pepperoni' }, { text: 'Margherita', callback: 'pizza_iwant_Margherita' }],
-            [{ text: 'zurücksetzen', callback: 'pizza_reset' }, { text: 'Nein, danke', callback: 'pizza_nothanks' }]
+            [{ text: 'zurücksetzen', callback: 'pizza_reset' }, /*{ text: 'Nein, danke', callback: 'pizza_nothanks' }*/]
         ],
         iwantList: 'Folgende Personen wollen Pizza: ',
         nothanksList: 'Folgende Personen wollen keine Pizza: ',
@@ -95,15 +82,17 @@ function handleFoodRequest(vote, ctx) {
     if (voteDatabase) {
         console.log(voteDatabase);
         const keyboard = Markup.inlineKeyboard(createButtonsForVote(vote));
-        ctx.telegram.sendMessage(voteDatabase.chatRoomId, ctx.from.first_name + questions[vote].question, keyboard.extra())
+        return ctx.telegram.sendMessage(voteDatabase.chatRoomId, ctx.from.first_name + questions[vote].question, keyboard.extra())
             .then((response) => {
                 const chatRoom = { 'active': true, 'votes': {}, 'type': vote, 'keyboardMessageId': response.message_id };
                 chatRooms[voteDatabase.chatRoomId] = chatRoom;
-                ctx.telegram.sendMessage(voteDatabase.chatRoomId, 'Niemand hat abgestimmt.').then((secondResponse) => {
+                return ctx.telegram.sendMessage(voteDatabase.chatRoomId, 'Niemand hat abgestimmt.').then((secondResponse) => {
                     chatRoom.messageId = secondResponse.message_id;
+                    return ctx.telegram.sendMessage(ctx.from.id, 'Deine Umfrage wurde in ' + voteDatabase.title + ' gestartet.', { reply_markup: { remove_keyboard: true } });
                 });
             });
-        ctx.telegram.sendMessage(ctx.from.id, 'Deine Umfrage wurde in ' + voteDatabase.title + ' gestartet.', { reply_markup: { remove_keyboard: true } });
+    } else {
+        return ctx.answerCbQuery();
     }
 }
 
@@ -158,9 +147,8 @@ function handleVoteAction(ctx, voteAction, param) {
         }
 
         if (chatRoom.lastMessage !== message) {
-            ctx.telegram.editMessageText(ctx.chat.id, chatRoom.messageId, null, message).then(() => {
-                chatRoom.lastMessage = message;
-            }).catch((error) => {
+            chatRoom.lastMessage = message;
+            ctx.telegram.editMessageText(ctx.chat.id, chatRoom.messageId, null, message).catch((error) => {
                 console.log(error);
             });
         }
@@ -168,8 +156,9 @@ function handleVoteAction(ctx, voteAction, param) {
     }
     else {
         ctx.telegram.sendMessage(ctx.from.id, 'In "' + ctx.chat.title + '" läuft keine aktive Umfrage!');
-    };
-};
+    }
+    ctx.answerCbQuery();
+}
 app.action(/pizza_([a-z]+)_?(.*)/, (ctx) => {
     handleVoteAction(ctx, ctx.match[1], ctx.match[2]);
 });
@@ -182,21 +171,4 @@ app.action('nothanks', (ctx) => {
     handleVoteAction(ctx, 'nothanks');
 });
 
-/*
-app.action('choose_pizza', (ctx) => {
-    handleFoodRequest('vote', ctx);
-});
-
-app.action('choose_subway', (ctx) => {
-    handleFoodRequest('vote', ctx);
-});
-
-app.action('choose_nothing', (ctx) => {
-    handleFoodRequest('vote', ctx);
-});
-
-app.on('callback_query', (ctx) => {
-    console.log(ctx.callbackQuery);
-});*/
-
-app.startPolling();
+module.exports = app;
