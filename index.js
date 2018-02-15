63908996fa3a02db1097c64eaa4d4b6daabd1afa
Patrick <patrick2003>
2018-02-15 21:51:23 +0100

    added grundid server

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const functions = require('firebase-functions');
 const express = require('express');
-const foodsupplybot = require('./foodsupplybot')(functions.config().bot.token);
+const foodsupplybot = require('./foodsupplybot')(process.env.BOT_TOKEN);
 
 const app = express();
 app.use(foodsupplybot.webhookCallback('/bot'));
-//foodsupplybot.telegram.setWebhook('https://us-central1-foodsupplybot.cloudfunctions.net/bot');
-exports.bot = functions.https.onRequest(app);
+foodsupplybot.telegram.setWebhook('https://bots.grundid.de/node/bot');
+app.listen(5000, () => {
+    console.log('Bot activated');
+});
