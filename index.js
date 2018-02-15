d7d22dafb253eb70faf9c4abefd369f37ab1b052
Adrian Stabiszewski <as@grundid.de>
2018-02-15 22:07:46 +0100

    set url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const foodsupplybot = require('./foodsupplybot')(process.env.BOT_TOKEN);
 
 const app = express();
-app.use(foodsupplybot.webhookCallback('/bot'));
+app.use(foodsupplybot.webhookCallback('/node/bot'));
 foodsupplybot.telegram.setWebhook('https://bots.grundid.de/node/bot');
 app.listen(5000, () => {
     console.log('Bot activated');
